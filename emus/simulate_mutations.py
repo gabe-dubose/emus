7a74b8a381b5f3d3269795281f88ec6f9219a31b
Gabe DuBose <gabe@Gabes-MacBook-Air.local>
2022-05-15 08:56:00 -0400

    Updated simulation script

--- emus/simulate_mutations.py
+++ emus/simulate_mutations.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 
 import argparse
-import re
 import random
 from datetime import date
 import time
+import pyfaidx
 
-#define command line arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--input', metavar = '<Input Genome in FASTA Format>', 
     help = "Genome sequence used to simulate null distribution of mutaitons")
@@ -16,56 +15,36 @@ parser.add_argument('-o', '--output', metavar = '<output_file.vcf>',
     help = "Handle that will be added to the output VCF file")
 args = parser.parse_args()
 
-#class to handle initial input genome sequence and simulate mutations
+
+#genome = pyfaidx.Fasta(args.input)
+#print(genome.keys())
+#print(genome['BX284602.5'][200:300])
+#print(len(genome['BX284602.5']))
+
 class GenomeSequence:
     def __init__(self, genome):
         self.genome = genome
     
-    #function to extract raw genomes sequences for each chromosome
-    def get_genome(self, genome):
+    def simulate_variants(self, fasta, number):
+
+        #read fasta file
+        genome = pyfaidx.Fasta(fasta)
+        sequences = list(genome.keys())
         
-        raw_sequence = {}
-        current_header = ''
-        with open(genome) as genome:
-            lines = genome.readlines()
-            for line in lines:
-                line = line.rstrip()
-                header_line = bool(re.match("^>", line))
-                if header_line == True:
-                    current_header = line.lstrip('>')
-                    current_header = current_header.split(' ')[0]
-                    raw_sequence[current_header] = ''
-                else:
-                    raw_sequence[current_header] = raw_sequence[current_header] + line.upper()
-
-        #get sequence length information to be written to VFC file later
-        sequence_data = {}
-        for seq in raw_sequence:
-            sequence_data[seq] = len(raw_sequence[seq])
-
-        sequences_information = (raw_sequence, sequence_data)
-        return sequences_information
-
-    #function to simualte mutations in genome
-    def simulate_mutations(self, sequence_dict, number):
         alphabet = ['A', 'T', 'G', 'C']
 
-        #get sequence ids
-        sequence_headers = []
-        for key in sequence_dict:
-            sequence_headers.append(key)
-        
-        #loop to generate random mutations
-        variant_calls = []
-        mutation_info = {}
         mutation_count = 0
+        mutation_info = {}
+        variant_calls = []
+
         while mutation_count < number:
-
-            variant = str(random.sample(alphabet, 1)).lstrip("['").rstrip("]'")
-            seqid = str(random.sample(sequence_headers, 1)).lstrip("['").rstrip("]'")
-            position = random.randint(0, len(sequence_dict[seqid]) - 1)
-            original =  sequence_dict[seqid][position]
-
+            variant = random.choices(alphabet, k=1)
+            variant = variant[0]
+            seqid = random.sample(sequences, 1)
+            seqid = seqid[0]
+            position = random.randint(0, len(genome[seqid]) - 1)
+            original = str(genome[seqid][position]).upper()
+        
             #add sequence id to info dictionary if not already there
             if seqid not in mutation_info:
                 mutation_info[seqid] = []
@@ -77,24 +56,27 @@ class GenomeSequence:
                 variant_calls.append(variant_call)
                 mutation_info[seqid].append(position)
                 mutation_count += 1
-        
+
         return variant_calls
 
-#function to write vcf file
-def write_vcf(variants, seq_data):
+    
+def write_vcf(fasta, variants):
+    #read fasta file
+    genome = pyfaidx.Fasta(fasta)
+    sequences = genome.keys()
+
+    #initialize output VCF
     today = date.today().strftime("%Y%m%d")
     with open(args.output, 'a') as outfile:
         outfile.write(f'##fileformat=VCFv4.3\n')
         outfile.write(f'##date={today}\n')
         outfile.write(f'##source=emus.v.1.0.0\n')
         outfile.write(f'##reference={args.input}\n')
-        
-        for seq in seq_data:
-            outfile.write(f'##contig=<ID={seq},lenght={seq_data[seq]}>\n')
+        for sequence in sequences:
+            outfile.write(f'##contig=<ID={sequence}, lenght = {len(genome[sequence])}>\n')
 
         outfile.write(f'#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\tSAMPLE\n')
 
-        #sorting variants by chromosome and position
         sorted_variants = sorted(variants, key=lambda x: (x[0], x[1]))
         for variant in sorted_variants:
             var_line = f'{variant[0]}\t{int(variant[1])+1}\t.\t{variant[2]}\t{variant[3]}\t.\t.\t.\tGT\t1\n'
@@ -103,21 +85,13 @@ def write_vcf(variants, seq_data):
 #define sequence objects
 sequence_object = GenomeSequence(args.input) 
 
-#extract raw sequences and sequences data
-print('Reading Sequences...')
-start_read = time.time()
-sequence_information = sequence_object.get_genome(args.input)
-raw_sequence = sequence_information[0]
-sequence_data = sequence_information[1]
-print(f'Done in {time.time() - start_read} seconds.')
-
 #simulate mutations
 print('Simulating mutations...')
 start_sim = time.time()
-simulation = sequence_object.simulate_mutations(raw_sequence, args.snp)
+simulation = sequence_object.simulate_variants(args.input, args.snp)
 print(f'Done in {time.time() - start_sim} seconds.')
 
 #write variant call file
 print('Writing variant call file...')
-write_vcf(simulation, sequence_data)
+write_vcf(args.input, simulation)
 print('Done.')
